19453804320201111248faf70224f7bd290767a3
ClementG91 <123742385+ClementG91@users.noreply.github.com>
2026-07-23 14:26:04 +0200

    add quant research package

---
+++ quant_research/__init__.py
@@ -0,0 +1,5 @@
+"""Leakage-resistant multi-asset quant research toolkit."""
+
+from .core import ResearchConfig, ResearchResult, run_walk_forward
+
+__all__ = ["ResearchConfig", "ResearchResult", "run_walk_forward"]
